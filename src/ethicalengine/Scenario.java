c137171267e26e3b4029d9888fa95d3a975f5e99
qiutingli <qiutingli.china@gmail.com>
2020-06-28 01:33:16 +1000

    Update output structure

--- src/ethicalengine/Scenario.java
+++ src/ethicalengine/Scenario.java
@@ -61,25 +61,27 @@ public class Scenario {
                 "# Scenario\n" +
                 "======================================\n" +
                 "Legal Crossing: " + (this.isLegalCrossing? "yes" : "no") + "\n" +
-                "Passengers (" + this.getPassengerCount() + ")\n");
+                "Passengers (" + this.getPassengerCount() + ")");
         for (Character character : this.passengers){
-            summary.append("-x ").append(character.toString()).append("\n");
+            summary.append("\n").append("- ").append(character.toString());
         }
-        summary.append("Pedestrians (").append(this.getPedestrianCount()).append(")\n");
+        summary.append("\n").append("Pedestrians (").append(this.getPedestrianCount()).append(")");
         for (Character character : this.pedestrians){
-            summary.append("- ").append(character.toString()).append("\n");
+            summary.append("\n").append("- ").append(character.toString());
         }
         return summary.toString();
     }
 
     // For test
     public static void main(String[] args) {
-        Character[] passengers = {new Animal("cat"), new Person(5, Person.Profession.NONE, Character.Gender.MALE,
-                Character.BodyType.OVERWEIGHT, false)};
-        Character[] pedestrians = {new Person(3, Person.Profession.NONE, Character.Gender.MALE,
-                Character.BodyType.AVERAGE, false),
-                new Person(20, Person.Profession.DOCTOR, Character.Gender.FEMALE,
-                Character.BodyType.AVERAGE, true)};
+        Character[] passengers = {new Person(20, Person.Profession.HOMELESS, Character.Gender.FEMALE,
+                Character.BodyType.ATHLETIC, false)};
+        Character[] pedestrians = {
+                new Person(120, Character.Gender.MALE,
+                Character.BodyType.ATHLETIC)
+//                new Person(20, Person.Profession.DOCTOR, Character.Gender.FEMALE,
+//                Character.BodyType.AVERAGE, true)
+        };
         Scenario scenario = new Scenario(passengers, pedestrians, true);
         System.out.println(scenario.toString());
     }
